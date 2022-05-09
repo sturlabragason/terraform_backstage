6fa8208662d2384a39ef80eeb75b6f6b24c9330f
Sturla Bragason <sbr@cloudeon.com>
2022-05-09 11:47:19 +0000

    dev

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ terraform {
 
 provider "azurerm" {
   subscription_id            = local.subscription_id
-  tenant_id                  = local.tenant_id
+  tenant_id                  = local.tenant
   client_secret              = var.az_client_secret
   client_id                  = local.appId
   skip_provider_registration = true
@@ -61,77 +61,77 @@ data "azurerm_client_config" "landingzone" {
 # # Resources - Resource Groups, Virtual Network, Storage Account, Container with Terraform Statefile, Keyvault containing Service Principal Credentials 
 # # ------------------------------------------------------
 
-# resource "azurerm_resource_group" "landingzone" {
-#   name     = local.resource_group_name_landingzone
-#   location = local.resource_location
-# }
-
-# resource "azurerm_kubernetes_cluster" "landingzone" {
-#   name                = local.aks_name_landingzone
-#   resource_group_name = azurerm_resource_group.landingzone.name
-#   location            = azurerm_resource_group.landingzone.location
-#   dns_prefix          = "${local.aks_name_landingzone}-1"
-
-#   default_node_pool {
-#     name       = "node1545f414"
-#     node_count = 1
-#     vm_size    = "Standard_D2_v2"
-#   }
-
-#   identity {
-#     type = "SystemAssigned"
-#   }
-#   # addon_profile {
-#   #   http_application_routing {
-#   #     enabled = true
-#   #   }
-#   #   azure_policy {
-#   #     enabled = true
-#   #   }
-#   # }
-# }
-
-# resource "azurerm_container_registry" "landingzone" {
-#   name                = local.acr_name
-#   resource_group_name = azurerm_resource_group.landingzone.name
-#   location            = azurerm_resource_group.landingzone.location
-#   sku                 = "Basic"
-# }
-
-# resource "azurerm_role_assignment" "landingzone" {
-#   principal_id                     = azurerm_kubernetes_cluster.landingzone.kubelet_identity[0].object_id
-#   role_definition_name             = "AcrPull"
-#   scope                            = azurerm_container_registry.landingzone.id
-#   skip_service_principal_aad_check = true
-# }
-
-# resource "azurerm_role_assignment" "lzpush" {
-#   principal_id                     = data.azurerm_client_config.landingzone.object_id
-#   role_definition_name             = "AcrPush"
-#   scope                            = azurerm_container_registry.landingzone.id
-#   skip_service_principal_aad_check = true
-# }
-
-resource "azurerm_storage_account" "main" {
-  name                     = local.storage_account_name_aks
-  resource_group_name      = azurerm_resource_group.landingzone.name
-  location                 = azurerm_resource_group.landingzone.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  min_tls_version          = "TLS1_2"
+resource "azurerm_resource_group" "landingzone" {
+  name     = local.resource_group_name_landingzone
+  location = local.resource_location
 }
 
-resource "azurerm_storage_share" "main" {
-  name                 = local.share_name
-  storage_account_name = azurerm_storage_account.main.name
-  quota                = 1
+resource "azurerm_kubernetes_cluster" "landingzone" {
+  name                = local.aks_name_landingzone
+  resource_group_name = azurerm_resource_group.landingzone.name
+  location            = azurerm_resource_group.landingzone.location
+  dns_prefix          = "${local.aks_name_landingzone}-1"
+
+  default_node_pool {
+    name       = "node1545f414"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+  # addon_profile {
+  #   http_application_routing {
+  #     enabled = true
+  #   }
+  #   azure_policy {
+  #     enabled = true
+  #   }
+  # }
 }
 
-resource "azurerm_storage_account" "demo" {
-  name                     = "demo${local.storage_account_name_aks}"
-  resource_group_name      = azurerm_resource_group.landingzone.name
-  location                 = azurerm_resource_group.landingzone.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  min_tls_version          = "TLS1_2"
+resource "azurerm_container_registry" "landingzone" {
+  name                = local.acr_name
+  resource_group_name = azurerm_resource_group.landingzone.name
+  location            = azurerm_resource_group.landingzone.location
+  sku                 = "Basic"
 }
+
+resource "azurerm_role_assignment" "landingzone" {
+  principal_id                     = azurerm_kubernetes_cluster.landingzone.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.landingzone.id
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_role_assignment" "lzpush" {
+  principal_id                     = data.azurerm_client_config.landingzone.object_id
+  role_definition_name             = "AcrPush"
+  scope                            = azurerm_container_registry.landingzone.id
+  skip_service_principal_aad_check = true
+}
+
+# resource "azurerm_storage_account" "main" {
+#   name                     = local.storage_account_name_aks
+#   resource_group_name      = azurerm_resource_group.landingzone.name
+#   location                 = azurerm_resource_group.landingzone.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
+#   min_tls_version          = "TLS1_2"
+# }
+
+# resource "azurerm_storage_share" "main" {
+#   name                 = local.share_name
+#   storage_account_name = azurerm_storage_account.main.name
+#   quota                = 1
+# }
+
+# resource "azurerm_storage_account" "demo" {
+#   name                     = "demo${local.storage_account_name_aks}"
+#   resource_group_name      = azurerm_resource_group.landingzone.name
+#   location                 = azurerm_resource_group.landingzone.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
+#   min_tls_version          = "TLS1_2"
+# }

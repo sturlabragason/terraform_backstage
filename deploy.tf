b52be859746f5f1f26c081a7a8a6788ed5f65f6e
Sturla Bragason <sbr@cloudeon.com>
2022-05-09 12:32:24 +0000

    dev

--- deploy.tf
+++ deploy.tf
@@ -1,13 +1,14 @@
 resource "null_resource" "build" {
   provisioner "local-exec" {
     command = <<EOF
-    ls
-    # export NODE_OPTIONS="--max-old-space-size=8192"
-    # cd backstage
-    # yarn install -g
-    # yarn install --frozen-lockfile
-    # yarn tsc
-    # yarn build
+        ls
+        nvm use 16
+        export NODE_OPTIONS="--max-old-space-size=8192"
+        cd backstage
+        yarn install -g
+        yarn install --frozen-lockfile
+        yarn tsc
+        yarn build
     EOF
   }
 }

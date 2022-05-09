db9762a9481da700dcc4a58f405b817e599a25d6
Sturla Bragason <sbr@cloudeon.com>
2022-05-09 13:04:37 +0000

    dev

--- deploy.tf
+++ deploy.tf
@@ -1,15 +1,15 @@
-resource "null_resource" "build" {
-  provisioner "local-exec" {
-    command     = <<-EOF
-        /bin/ls
-        nvm use 16
-        export NODE_OPTIONS="--max-old-space-size=8192"
-        cd backstage
-        yarn install -g
-        yarn install --frozen-lockfile
-        yarn tsc
-        yarn build
-    EOF
-    interpreter = ["sh", "-c"]
-  }
-}
+# resource "null_resource" "build" {
+#   provisioner "local-exec" {
+#     command     = <<-EOF
+#         /bin/ls
+#         nvm use 16
+#         export NODE_OPTIONS="--max-old-space-size=8192"
+#         cd backstage
+#         yarn install -g
+#         yarn install --frozen-lockfile
+#         yarn tsc
+#         yarn build
+#     EOF
+#     interpreter = ["sh", "-c"]
+#   }
+# }

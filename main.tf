93db44482a67f84de29485003f180190cd40519a
Sturla Bragason <sturla.bragason@devoteam.com>
2022-06-17 06:56:35 +0000

    first cluster then k8s

--- main.tf
+++ main.tf
@@ -46,14 +46,14 @@ provider "azurerm" {
   features {}
 }
 
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.landingzone.kube_config.0.host
-  username               = azurerm_kubernetes_cluster.landingzone.kube_config.0.username
-  password               = azurerm_kubernetes_cluster.landingzone.kube_config.0.password
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.landingzone.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.landingzone.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.landingzone.kube_config.0.cluster_ca_certificate)
-}
+# provider "kubernetes" {
+#   host                   = azurerm_kubernetes_cluster.landingzone.kube_config.0.host
+#   username               = azurerm_kubernetes_cluster.landingzone.kube_config.0.username
+#   password               = azurerm_kubernetes_cluster.landingzone.kube_config.0.password
+#   client_certificate     = base64decode(azurerm_kubernetes_cluster.landingzone.kube_config.0.client_certificate)
+#   client_key             = base64decode(azurerm_kubernetes_cluster.landingzone.kube_config.0.client_key)
+#   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.landingzone.kube_config.0.cluster_ca_certificate)
+# }
 
 
 data "azurerm_client_config" "landingzone" {
